5ff1b0fdb17eecc6fe89f7c232a2d401f53f1c12
Gharbi Mohammed <simo163@hotmail.co.uk>
2015-01-30 16:34:42 +0100

    Use fixtures

--- js/models/todo.js
+++ js/models/todo.js
@@ -2,3 +2,22 @@ Todos.Todo = DS.Model.extend({
 	title: DS.attr('string'),
 	isCompleted: DS.attr('boolean')
 });
+
+
+Todos.Todo.FIXTURES = [
+ {
+   id: 1,
+   title: 'Learn Ember.js guides',
+   isCompleted: true
+ },
+ {
+   id: 2,
+   title: 'Take it into RoR project',
+   isCompleted: false
+ },
+ {
+   id: 3,
+   title: 'Build your best app',
+   isCompleted: false
+ }
+];
